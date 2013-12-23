34e590d6d55c2fd8e2f7a65197b3957a055c477a
Damien Sueur <damien@scopely.com>
2013-12-22 22:26:55 -0800

    Added HTML files

---
+++ Assets/Scripts/_HTTP.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _HTTP : MonoBehaviour
+{
+    private _CP CP;
+    private GUIText guiInfo;
+
+    // Use this for initialization
+    void Start()
+    {
+        CP = GameObject.Find("_CP").GetComponent<_CP>();
+        guiInfo = GameObject.Find("GUI_Info").guiText;
+
+        //// Connect
+        //Application.ExternalEval(
+        //    "alert('connected');"
+        //);
+    }
+	
+    //// Update is called once per frame
+    //void Update ()
+    //{
+	    
+    //}
+
+    void Connect(string msg)
+    {
+        guiInfo.text = msg;
+    }
+}
